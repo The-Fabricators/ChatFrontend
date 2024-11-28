949768677f08430a1c24ab5e3170b2f50470ea96
Gabz047 <gabe1612mail@gmail.com>
2024-11-28 15:46:14 -0300

    FEAT: Created Project in Vue and Added necessary packages

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1 class="text">Hello World!</h1>
+  <span class="mdi mdi-account-credit-card icon"></span>
+  <RouterView />
+</template>
+
+<style scoped>
+@import '@/assets/sass/_home.scss';
+</style>
