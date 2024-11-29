25c3cc4459fff7050c9606594f747be3b5e633a5
Joaovictor23t4 <joaovictor239090@gmail.com>
2024-11-29 09:20:04 -0300

    FEAT: Create init routes #14

---
+++ src/components/chat/GeneralChat.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <div>GENERAL</div>
+</template>
+
+<style scoped>
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,21 @@
 import BtnStroke from "./globals/BtnStroke.vue";
 import HeaderHome from "./home/HeaderHome.vue";
+import GeneralChat from "@/components/chat/GeneralChat.vue";
+import NatureChat from "@/components/chat/NatureChat.vue";
+import MathematicChat from "@/components/chat/MathematicChat.vue";
+import HumansChat from "@/components/chat/HumansChat.vue";
+import LanguagesChat from "@/components/chat/LanguagesChat.vue";
+import RedactionChat from "@/components/chat/RedactionChat.vue";
+import SubscriptionPlan from "@/views/subscriptionPlan/SubscriptionPlanView.vue";
 
 export {
+    GeneralChat,
+    NatureChat,
+    MathematicChat,
+    HumansChat,
+    LanguagesChat,
+    RedactionChat,
+    SubscriptionPlan,
     BtnStroke,
     HeaderHome
-}
+};
