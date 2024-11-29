85610b9f2cee35ee7c59a74054deae43fe72483e
matheusmichalski <matheus.michalskifc@gmail,com>
2024-11-29 16:08:25 -0300

    FIX:Create responsive cards of buy plans

--- src/components/subscriptionPlan/FreeSubscription.vue
+++ src/components/subscriptionPlan/FreeSubscription.vue
@@ -1,15 +1,30 @@
 <script setup>
-import defaultBtn from '../globals/buttons/defaultBtn.vue';
+import defaultBtn from '../global/buttons/defaultBtn.vue'
 </script>
 <template>
-    <div class="subscription-container free">
-        <div class="text"><h1>FREE</h1>
-        <h2>Plano Padrão</h2>
-        <p>Chat de Perguntas e respostas com IA’s</p>
-        <p class="space-last">5 Especialistas</p></div>
-        <defaultBtn text="Ativo" background="#2971201A" color="#000000" />
+  <div class="subscription-container free">
+    <div class="text">
+      <h1>FREE</h1>
+      <h2>Plano Padrão</h2>
+      <p>Chat de Perguntas e respostas com IA’s</p>
+      <p class="space-last">5 Especialistas</p>
     </div>
+    <span class="line"></span>
+    <p><span class="mdi mdi-star"></span>Chat de Perguntas e respostas com IA’s</p>
+    <p><span class="mdi mdi-star"></span>5 Especialistas</p>
+    <p><span class="mdi mdi-star"></span>IA’s com treinamento avançado</p>
+    <p><span class="mdi mdi-star"></span>Responder perguntas enviadas por imagem</p>
+    <p><span class="mdi mdi-star"></span>Responder perguntas enviadas por áudio</p>
+    <p class="space-last">
+      <span class="mdi mdi-star"></span>Gerar exercícios para o usuário fazer
+    </p>
+    <defaultBtn text="Ativo" background="#2971201A" color="#000000" width="370px" />
+  </div>
+  <div class="text">
+    <h1>PREMIUM</h1>
+    <h2>R$ 50,00 / mês</h2>
+  </div>
 </template>
 <style scoped>
-@import "@/assets/sass/_subscription.scss";
+@import '@/assets/sass/_subscription.scss';
 </style>
