2fba40d5e03346e13a42b54cd1887e54148b16eb
Gabz047 <gabe1612mail@gmail.com>
2024-11-29 15:21:30 -0300

    CHORE: Fixed rebase erros

--- src/components/index.js
+++ src/components/index.js
@@ -8,7 +8,7 @@ import HumansChat from "@/components/chat/HumansChat.vue";
 import LanguagesChat from "@/components/chat/LanguagesChat.vue";
 import RedactionChat from "@/components/chat/RedactionChat.vue";
 import SubscriptionPlan from "@/views/subscriptionPlan/SubscriptionPlanView.vue";
-import BtnStroke from "./globals/BtnStroke.vue";
+import BtnStroke from "./global/header/BtnStroke.vue";
 import HeaderHome from "./home/HeaderHome.vue";
 
 
