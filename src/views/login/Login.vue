fc28883c40143c55208164d51813c2546820bfba
julia <juliafu713@gmail.com>
2024-11-30 03:15:39 -0300

    STYLE: Login style changed #83

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,5 +1,7 @@
 <template>
+  <div class="content">
   <passage-auth :app-id="appId"></passage-auth>
+</div>
 </template>
 
 <script setup>
@@ -7,3 +9,7 @@ import '@passageidentity/passage-elements/passage-auth'
 
 const appId = import.meta.env.VITE_PASSAGE_APP_ID
 </script>
+
+<style scoped>
+@import '@/assets/sass/global/_login.scss';
+</style>
