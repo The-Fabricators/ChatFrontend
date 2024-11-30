c515444828ea7f1dd3024589371e76b67ba47be3
jonatassilvaperaza@gmail.com <jonatassilvaperaza@gmail.com>
2024-11-30 02:01:17 -0300

    FEAT: remove Mercado Pago dependency; update PaymentView to use new payment brick structure [Refs: #33]

--- src/views/global/PaymentView.vue
+++ src/views/global/PaymentView.vue
@@ -1,77 +1,74 @@
 <template>
-  <div id="paymentBrick_container"></div>
+  <div id="cardPaymentBrick_container"></div>
+  <div id="bankTransferBrick_container"></div>
 </template>
+
 <script setup>
 import axios from 'axios';
 import { onMounted } from 'vue';
+import { useAuthStore } from '@/stores/auth';
+import router from '@/router';
 
-const Public_Key = import.meta.env.VITE_MP_PUBLIC_KEY
-
-const mp = new window.MercadoPago(Public_Key, {
-  locale: 'pt-BR',
-});
+const api_key = import.meta.env.VITE_MERCADOPAGO_PUBLIC_KEY;
+const authStore = useAuthStore();
+const mp = new window.MercadoPago(api_key);
 const bricksBuilder = mp.bricks();
 
 const renderCardPaymentBrick = async (bricksBuilder) => {
   try {
     const settings = {
-        initialization: {
-          amount: 1,
-          preferenceId: "<PREFERENCE_ID>",
-          payer: {
-            firstName: "",
-            lastName: "",
-            email: "",
+      initialization: {
+        amount: 1, // valor do pagamento a ser processado
+        payer: {
+          type: 'cpf',
+          number: '82133948953',
+        },
+      },
+      customization: {
+        visual: {
+          style: {
+            theme: 'bootstrap', // 'default' | 'dark' | 'bootstrap' | 'flat'
           },
         },
-customization: {
-          visual: {
-            style: {
-              theme: "bootstrap",
-            },
-          },
-          paymentMethods: {
-            creditCard: "all",
-										debitCard: "all",
-										ticket: "all",
-										atm: "all",
-										bankTransfer: "all",
-            maxInstallments: 1
-          },
+        paymentMethod: {
+          creditCard: 'all',
+          bankTransfer: 'all',
+          minInstallments: 1,
+          maxInstallments: 12,
         },
+      },
       callbacks: {
         onSubmit: async (cardFormData) => {
           try {
             console.log(cardFormData);
             await axios.post('http://172.25.44.90:8000/api/payments/', {
-                token: cardFormData?.token,
-                issuer_id: cardFormData?.issuer_id,
+                token: cardFormData.token,
+                issuer_id: cardFormData.issuer_id,
                 description: cardFormData.description || 'Pagamento de teste',
                 payment_method_id: cardFormData.payment_method_id,
                 transaction_amount: cardFormData.transaction_amount,
-                installments: cardFormData?.installments,
-                payment_email: cardFormData.payer?.email,
-                payment_identification_number: cardFormData.payer?.identification.number,
-                payment_identification_type: cardFormData.payer?.identification.type,
-                user: 1,
+                installments: cardFormData.installments,
+                payment_email: cardFormData.payer.email,
+                payment_identification_number: cardFormData.payer.identification.number,
+                payment_identification_type: cardFormData.payer.identification.type,
+                user: authStore.user?.id,
               })
+              alert("Pagamento realizado com sucesso!");
+              router.push('/')
           } catch (error) {
             console.error(error);
-            alert("fudeu: " + error.message);
             throw error; // Repassa o erro para callbacks, se necessário
           }
         },
         onReady: () => {
-          alert("tá pronto");
         },
         onError: (error) => {
           console.error(error);
-          alert("deu pau");
         },
       },
     };
 
-    await bricksBuilder.create('payment', 'paymentBrick_container', settings);
+    await bricksBuilder.create('cardPayment', 'cardPaymentBrick_container', settings);
   } catch (error) {
     console.error("Erro ao renderizar o cardPaymentBrick:", error);
     alert("Erro ao inicializar o pagamento.");
@@ -87,6 +84,3 @@ onMounted(async () => {
   }
 });
 </script>
-<style scoped>
-
-</style>
